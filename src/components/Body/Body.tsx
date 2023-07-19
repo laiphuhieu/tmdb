a08ec9fbb5592d64c89b2e97d03f18085b6e3629
laiphuhieu <laiphuhieu123@gmail.com>
2023-07-20 01:20:43 +0700

    body #1

---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import New from "./New/New";
+
+const Body = () => {
+  return (
+    <main className="w-[100%] flex justify-center mt-[64px] content-start bg-[#fff]">
+      <New />
+    </main>
+  );
+};
+
+export default Body;
--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,3 +1,4 @@
+import Body from "@/components/Body/Body";
 import Header from "@/components/Header/Header";
 import React from "react";
 
@@ -5,6 +6,7 @@ const Movies = () => {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 };
