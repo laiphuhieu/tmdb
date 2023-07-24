38e71bdaf5e94e73ccbacc4cb58b4f8e60f387fc
laiphuhieu <laiphuhieu123@gmail.com>
2023-07-24 09:03:37 +0700

    body #3

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import New from "./New/New";
 import Trending from "./Trending/Trending";
+import Trailers from "./Trailers/Trailers";
 
 const Body = () => {
   return (
     <main className="w-[100%] flex justify-center flex-wrap mt-[64px] content-start bg-[#fff]">
       <New />
       <Trending />
+      <Trailers />
     </main>
   );
 };
