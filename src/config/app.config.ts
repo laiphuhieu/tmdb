64f27e8e5a76147592762d664f89d76aae780cc0
laiphuhieu <laiphuhieu123@gmail.com>
2023-07-26 02:32:57 +0700

    fix body #1

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = (process.env.REACT_APP_BASE_URL || '') as string;
 export const API_TOKEN = (process.env.REACT_APP_API_TOKEN || '') as string;
+export const PATH_IMAGE = (process.env.REACT_APP_PATH_IMAGE || '') as string;
--- src/services/trendingService.tsx
+++ src/services/trendingService.tsx
@@ -1,16 +1,19 @@
-import { AxiosPromise } from "axios";
-
-import { Trending } from "@/types/trending";
 import axiosInstance from "./apiService";
+import { Trending } from "@/types/trending";
 
 const trendingService = {
   getTrendingDay(token: string): Promise<Trending> {
     return axiosInstance(token).get("/trending/movie/day?language=vi");
   },
 
-  getTrendingWeek(token: string): AxiosPromise {
-    return axiosInstance(token).get<Trending>(
-      "/trending/movie/week?language=vi"
+  getTrendingWeek(token: string): Promise<Trending> {
+    return axiosInstance(token).get("/trending/movie/week?language=vi");
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getTrendingDayById(token: string, id: any) {
+    return axiosInstance(token).get(
+      `/trending/movie/day/${id}?language=vi&append_to_response=videos`
     );
   },
 };
