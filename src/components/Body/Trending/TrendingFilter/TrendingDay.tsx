85c3ee5c47413fe1d66311e04f22c04aaad1b859
laiphuhieu <laiphuhieu123@gmail.com>
2023-07-25 10:34:20 +0700

    setup api service

--- src/components/Body/Trending/TrendingFilter/TrendingDay.tsx
+++ src/components/Body/Trending/TrendingFilter/TrendingDay.tsx
@@ -1,112 +1,111 @@
 import React, { useCallback, useState, useEffect } from "react";
-import axios from "axios";
+
 import { Link } from "react-router-dom";
 import Tippy from "@tippyjs/react";
 
 import DropdownMore from "@/components/Dropdown/DropdownMore/DropdownMore";
 import styles from "./TrendingFilter.module.scss";
-
-interface Item {
-  title: string;
-  id: number;
-  vote_average: number;
-  release_date: string;
-  poster_path: string;
-}
+import trendingService from "@/services/trendingService";
+import { TrendingResult } from "@/types/trending";
+import { API_TOKEN } from "@/config/app.config";
 
 const TrendingDay = () => {
-  const [trendingDay, setTrendingDay] = useState([]);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [trendingDay, setTrendingDay] = useState<TrendingResult[]>([]);
 
-  const getItemDay = useCallback(() => {
-    axios
-      .get("https://api.themoviedb.org/3/trending/movie/day", {
-        params: { language: "vi" },
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMGQ4NzdiOGY2Y2Q5NjNhNjU5NGQ2OTFjNzdkMjc4MyIsInN1YiI6IjY0YWY2NWY3M2UyZWM4MDE0ZjRhZTczMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZY3Kb416Ey4F3SVm5FeHosO8eD9AdDYOYbw4N_jieUE`,
-        },
-      })
-      .then((res) => {
-        setTrendingDay(res.data.results);
-      });
+  const getTrendingDayData = useCallback(async () => {
+    try {
+      const trendingDayData = await trendingService.getTrendingDay(API_TOKEN);
+      const trendingDayDataResults = trendingDayData.results;
+      setTrendingDay(trendingDayDataResults);
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
   useEffect(() => {
-    getItemDay();
-    // getItemWeek();
-  }, [getItemDay]);
+    getTrendingDayData();
+  }, [getTrendingDayData]);
+
   return (
     <div className={`${styles["scroll-wrap"]}`}>
       <div className={`${styles["scroll-loaded"]} ${styles["scrollbar"]}`}>
-        {trendingDay.map((result: Item, id: number) => {
-          return (
-            <div key={id} className={` ${styles["card-style-1"]}`}>
-              <div className={`${styles["card-img"]}`}>
-                <div className={`${styles["wrapper"]}`}>
-                  <Link
-                    to={`/movie/${result.id}`}
-                    title={result.title}
-                    className="w-[100%] h-[100%] inline-block"
-                  >
-                    <img
-                      loading="lazy"
-                      src={`https://www.themoviedb.org/t/p/w220_and_h330_face${result.poster_path}`}
-                      alt={result.title}
-                      className="w-[100%] h-[100%] inline-block "
-                    />
-                  </Link>
-                </div>
-                <Tippy
-                  content={<DropdownMore />}
-                  placement="bottom"
-                  animation="fade"
-                  theme="light"
-                  arrow={false}
-                  trigger="click"
-                  appendTo="parent"
-                  interactive={true}
-                >
-                  <div className={`${styles["options"]}`}>
-                    <div>
-                      <button
-                        aria-haspopup="true"
-                        className={`${styles["more-btn"]}`}
-                      ></button>
+        {trendingDay && trendingDay.length > 0 ? (
+          <>
+            {trendingDay.map((result: TrendingResult, id: number) => {
+              return (
+                <div key={id} className={` ${styles["card-style-1"]}`}>
+                  <div className={`${styles["card-img"]}`}>
+                    <div className={`${styles["wrapper"]}`}>
+                      <Link
+                        to={`/movie/${result.id}`}
+                        title={result.title}
+                        className="w-[100%] h-[100%] inline-block"
+                      >
+                        <img
+                          loading="lazy"
+                          src={`https://www.themoviedb.org/t/p/w220_and_h330_face${result.poster_path}`}
+                          alt={result.title}
+                          className="w-[100%] h-[100%] inline-block "
+                        />
+                      </Link>
                     </div>
-                  </div>
-                </Tippy>
-              </div>
-
-              <div className={`${styles["card-content"]}`}>
-                <div className={`${styles["consensus"]}`}>
-                  <div className={`${styles["outer-ring"]}`}>
-                    <div className={`${styles["user-score"]}`}>
-                      <div className={`${styles["percent"]}`}>
-                        <span>{Math.round(result.vote_average) * 10}</span>
+                    <Tippy
+                      content={<DropdownMore />}
+                      placement="bottom"
+                      animation="fade"
+                      theme="light"
+                      arrow={false}
+                      trigger="click"
+                      appendTo="parent"
+                      interactive={true}
+                    >
+                      <div className={`${styles["options"]}`}>
+                        <div>
+                          <button
+                            aria-haspopup="true"
+                            className={`${styles["more-btn"]}`}
+                          ></button>
+                        </div>
                       </div>
-                      <canvas
-                        height="34"
-                        width="34"
-                        className={`${styles["canvas"]}`}
-                      ></canvas>
-                    </div>
+                    </Tippy>
                   </div>
-                </div>
-                <h2>
-                  <Link
-                    to={`/movie/${result.id}`}
-                    title={result.title}
-                    className={`${styles["content-title"]}`}
-                  >
-                    {result.title}
-                  </Link>
-                </h2>
-                <p>{result.release_date}</p>
-              </div>
 
-              <div className={`${styles["card-hover"]}`}></div>
-            </div>
-          );
-        })}
+                  <div className={`${styles["card-content"]}`}>
+                    <div className={`${styles["consensus"]}`}>
+                      <div className={`${styles["outer-ring"]}`}>
+                        <div className={`${styles["user-score"]}`}>
+                          <div className={`${styles["percent"]}`}>
+                            <span>{Math.round(result.vote_average) * 10}</span>
+                          </div>
+                          <canvas
+                            height="34"
+                            width="34"
+                            className={`${styles["canvas"]}`}
+                          ></canvas>
+                        </div>
+                      </div>
+                    </div>
+                    <h2>
+                      <Link
+                        to={`/movie/${result.id}`}
+                        title={result.title}
+                        className={`${styles["content-title"]}`}
+                      >
+                        {result.title}
+                      </Link>
+                    </h2>
+                    <p>{result.release_date}</p>
+                  </div>
+
+                  <div className={`${styles["card-hover"]}`}></div>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          "no data"
+        )}
       </div>
     </div>
   );
---
+++ src/config/app.config.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = (process.env.REACT_APP_BASE_URL || '') as string;
+export const API_TOKEN = (process.env.REACT_APP_API_TOKEN || '') as string;
