cd78c95ffeb283fd859d00b25ddb003031f97dbb
laiphuhieu <laiphuhieu123@gmail.com>
2023-07-28 15:37:26 +0700

    fix body #3

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = (process.env.REACT_APP_BASE_URL || '') as string;
 export const API_TOKEN = (process.env.REACT_APP_API_TOKEN || '') as string;
+export const API_KEY = (process.env.REACT_APP_API_KEY || '') as string
 export const PATH_IMAGE = (process.env.REACT_APP_PATH_IMAGE || '') as string;
