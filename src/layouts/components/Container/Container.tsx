b4b960aa5b25c2c8f4d46ea50bcbe9b8f5791a65
PhuHieu <>
2024-01-23 10:50:33 +0700

    Reset Password Page

--- src/layouts/components/Container/Container.tsx
+++ src/layouts/components/Container/Container.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
 interface ContainerProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const Container = ({children}
-    : ContainerProps) => {
-    return (
-        <div className='flex w-full max-w-[1300px] mx-auto my-0'>
-            {children}
-        </div>
-    )
-}
+const Container = ({ children }: ContainerProps) => {
+  return (
+    <div className="flex w-full max-w-[1300px] mx-auto my-0 justify-center">
+      {children}
+    </div>
+  );
+};
 
-export default Container
+export default Container;
